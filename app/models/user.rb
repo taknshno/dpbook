4581fbd2ab6f71047d35f5df6cce4a1badaf4952
Taiki Nishino <tak.nshno.dic@gmail.com>
2023-04-05 05:25:17 +0900

    Add poster name display

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  has_many :pictures
 end
