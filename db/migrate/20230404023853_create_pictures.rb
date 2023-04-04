b6376ce2aa8df267a9b7310f5f6bb299e90a2945
Taiki Nishino <tak.nshno.dic@gmail.com>
2023-04-04 14:47:28 +0900

    Initial migration

--- db/migrate/20230404023853_create_pictures.rb
+++ db/migrate/20230404023853_create_pictures.rb
@@ -1,7 +1,8 @@
 class CreatePictures < ActiveRecord::Migration[6.1]
   def change
     create_table :pictures do |t|
-
+      t.text :image
+      t.text :content
       t.timestamps
     end
   end
