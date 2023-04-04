09a661f2966414896e1bc29fd7709d1a28b9299b
Taiki Nishino <tak.nshno.dic@gmail.com>
2023-04-05 04:48:44 +0900

    Add session feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :pictures do
     collection do
       post :confirm
     end
   end
 
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
 end
