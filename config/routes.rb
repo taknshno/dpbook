7f8b2741bce9eeb4064e479ec5be2993815f855c
Taiki Nishino <tak.nshno.dic@gmail.com>
2023-04-20 15:11:25 +0900

    Set root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  root 'pictures#index'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
-  
+
   resources :pictures do
     collection do
       post :confirm
